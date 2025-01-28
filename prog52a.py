e972432ef7aff3533d2655dbeaf71fd1830fe702
DbleTrouble <dbltble@gmail.com>
2025-01-28 20:57:28 +0000

    commit

---
+++ prog52a.py
@@ -0,0 +1,6 @@
+length = int(input("Enter Length: "))
+width  = int(input("Enter Width: "))
+area =  width * length
+perim = 2 * length + 2 * width
+print("Area: ", area)
+print("Perimeter: ", perim)
