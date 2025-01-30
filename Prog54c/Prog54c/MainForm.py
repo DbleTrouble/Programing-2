dc54c64777ae530cc2b90adbab8442872c07e6d5
DbleTrouble <dbltble@gmail.com>
2025-01-30 14:45:24 -0600

    working

---
+++ Prog54c/Prog54c/MainForm.py
@@ -0,0 +1,146 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+        self._textBox1 = System.Windows.Forms.TextBox()
+        self._label5 = System.Windows.Forms.Label()
+        self._label4 = System.Windows.Forms.Label()
+        self._label3 = System.Windows.Forms.Label()
+        self._label2 = System.Windows.Forms.Label()
+        self._label1 = System.Windows.Forms.Label()
+        self._button3 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self._button1 = System.Windows.Forms.Button()
+        self.SuspendLayout()
+        # 
+        # textBox1
+        # 
+        self._textBox1.Location = System.Drawing.Point(245, 19)
+        self._textBox1.Name = "textBox1"
+        self._textBox1.Size = System.Drawing.Size(215, 20)
+        self._textBox1.TabIndex = 17
+        # 
+        # label5
+        # 
+        self._label5.BackColor = System.Drawing.SystemColors.ActiveCaption
+        self._label5.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._label5.Location = System.Drawing.Point(245, 172)
+        self._label5.Name = "label5"
+        self._label5.Size = System.Drawing.Size(215, 40)
+        self._label5.TabIndex = 16
+        self._label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        # 
+        # label4
+        # 
+        self._label4.BackColor = System.Drawing.SystemColors.ActiveCaption
+        self._label4.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._label4.Location = System.Drawing.Point(245, 84)
+        self._label4.Name = "label4"
+        self._label4.Size = System.Drawing.Size(215, 40)
+        self._label4.TabIndex = 15
+        self._label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        # 
+        # label3
+        # 
+        self._label3.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._label3.Location = System.Drawing.Point(9, 172)
+        self._label3.Name = "label3"
+        self._label3.Size = System.Drawing.Size(139, 40)
+        self._label3.TabIndex = 14
+        self._label3.Text = "Circumference"
+        self._label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        # 
+        # label2
+        # 
+        self._label2.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._label2.Location = System.Drawing.Point(28, 84)
+        self._label2.Name = "label2"
+        self._label2.Size = System.Drawing.Size(120, 40)
+        self._label2.TabIndex = 13
+        self._label2.Text = "Area"
+        self._label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        # 
+        # label1
+        # 
+        self._label1.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._label1.Location = System.Drawing.Point(28, 7)
+        self._label1.Name = "label1"
+        self._label1.Size = System.Drawing.Size(120, 40)
+        self._label1.TabIndex = 12
+        self._label1.Text = "Radius"
+        self._label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        # 
+        # button3
+        # 
+        self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._button3.Location = System.Drawing.Point(533, 169)
+        self._button3.Name = "button3"
+        self._button3.Size = System.Drawing.Size(224, 47)
+        self._button3.TabIndex = 11
+        self._button3.Text = "Exit"
+        self._button3.UseVisualStyleBackColor = True
+        self._button3.Click += self.Button3Click
+        # 
+        # button2
+        # 
+        self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._button2.Location = System.Drawing.Point(533, 81)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(224, 47)
+        self._button2.TabIndex = 10
+        self._button2.Text = "Clear"
+        self._button2.UseVisualStyleBackColor = True
+        self._button2.Click += self.Button2Click
+        # 
+        # button1
+        # 
+        self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._button1.Location = System.Drawing.Point(533, 4)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(224, 47)
+        self._button1.TabIndex = 9
+        self._button1.Text = "Caculate"
+        self._button1.UseVisualStyleBackColor = True
+        self._button1.Click += self.Button1Click
+        # 
+        # MainForm
+        # 
+        self.BackColor = System.Drawing.Color.FromArgb(255, 224, 192)
+        self.ClientSize = System.Drawing.Size(774, 240)
+        self.Controls.Add(self._textBox1)
+        self.Controls.Add(self._label5)
+        self.Controls.Add(self._label4)
+        self.Controls.Add(self._label3)
+        self.Controls.Add(self._label2)
+        self.Controls.Add(self._label1)
+        self.Controls.Add(self._button3)
+        self.Controls.Add(self._button2)
+        self.Controls.Add(self._button1)
+        self.Name = "MainForm"
+        self.Text = "Prog54c"
+        self.ResumeLayout(False)
+        self.PerformLayout()
+
+
+    def Button1Click(self, sender, e):
+        Radius = float(self._textBox1.Text)
+        Pi = int(3.14159)
+        Area = Pi*(Radius**2)
+        Circumference = 2*Pi*Radius
+        self._label4.Text = str(round(Area, 3))
+        self._label5.Text = str(round(Circumference, 3))
+
+    def Button2Click(self, sender, e):
+        self._label4.Text = ""
+        self._label5.Text = ""
+        self._textBox1.Text = ""
+
+    def Button3Click(self, sender, e):
+        Application.Exit()
