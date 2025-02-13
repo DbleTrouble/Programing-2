88a17c98721368f902964c0af180707b02d7ad7a
DbleTrouble <dbltble@gmail.com>
2025-02-13 15:09:38 -0600

    working

---
+++ VoidFunctions.py
@@ -0,0 +1,23 @@
+from CalcFunctions import *
+
+def doThing():
+    print("Hello, World!")
+
+
+def printNums() -> None:
+    for i in range(10):
+        print("i:", i)
+    pass
+
+def main():
+    doThing()
+    printNums()
+    a = add(1, 2)
+    q, r = divmod2(5, 10)
+    print(a)
+    print(q, r)
+    pass
+
+
+if __name__ == "__main__":
+    main()
