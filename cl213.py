c7b08c8226910e1196682a5422a3bb4d5613658b
DbleTrouble <dbltble@gmail.com>
2025-03-12 15:28:42 -0500

    working

--- cl213.py
+++ cl213.py
@@ -7,9 +7,9 @@ class ElectricBill:
         if 0 < self.kwh <= 2000:
             self.cost += self.kwh * .07
         elif 2000 < self.kwh <= 10000:
-            self.cost += self.kwh * .05
+            self.cost += (2000 * .07) + ((self.kwh - 2000) * .05)
         elif self.kwh > 10000:
-            self.cost += self.kwh * .04
+            self.cost += (2000 * .07) ((self.kwh - 12000) * .04)
 
     def __str__(self):
         return f"The cost of {self.kwh} is ${self.cost:.2f}"
