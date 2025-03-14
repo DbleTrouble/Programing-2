267e8896cccc8b14e20aa52c2d8cc89584625df4
DbleTrouble <dbltble@gmail.com>
2025-03-14 15:28:37 -0500

    working

---
+++ LP 3-7.py
@@ -0,0 +1,15 @@
+def main():
+    def calculate(self):
+        a = Int1 / Int2
+        b = Int1 % Int2
+
+        c = Int2 / Int1
+        d = Int2 % Int1
+
+    Int1 = float(input("Integer 1: "))
+    Int2 = float(input("Integer 2: "))
+    pass
+
+
+if __name__ == "__main__":
+    main()
