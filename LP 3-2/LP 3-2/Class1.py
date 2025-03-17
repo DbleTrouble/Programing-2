c088ef56a2bfc63d1782ed92b58080aad2047069
DbleTrouble <dbltble@gmail.com>
2025-03-17 15:28:20 -0500

    working

---
+++ LP 3-2/LP 3-2/Class1.py
@@ -0,0 +1,9 @@
+﻿
+class Class1:
+    def __init__(self, diameter, cost):
+        self.diameter = diameter
+        self.cost = cost
+        
+        def calculate_cost():
+            self.cost = (0.05 * self.diameter * self.diameter) + 0.75 + 1.00
+        pass
