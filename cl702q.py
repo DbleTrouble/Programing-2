30da6237b0935d954f488dbcc4b9d5bda924a4f4
DbleTrouble <dbltble@gmail.com>
2025-04-10 15:28:15 -0500

    working

--- cl702q.py
+++ cl702q.py
@@ -1,25 +1,25 @@
 class Vehicle:
-    def __init__(self, n, ln):
-        self._name = n
-        self._last = ln
+    def __init__(self, na, ml):
+        self._name = na
+        self._mile = ml
 
-    def get_name(self):
-        return self._first + " " + self._last
+    def get_car(self):
+        return self._name + " " + self._mile
 
 
 class Car(Vehicle):
-    def __init__(self, fn, ln, gpa):
-        super().__init__(fn, ln)  # or Person.__init__(fn, ln)
-        self.gpa = gpa
+    def __init__(self, na, ml, mw):
+        super().__init__(na, ml)  # or Person.__init__(fn, ln)
+        self.mw = mw
 
 
 class Truck(Vehicle):
-    def __init__(self, fn, ln, num_stu):
-        super().__init__(fn, ln)
-        self.num_stu = num_stu
+    def __init__(self, na, ml, mpd):
+        super().__init__(na, ml)
+        self.mpd = mpd
 
 
 class Bus(Vehicle):
-    def __init__(self, fn, ln, fav_word):
-        super().__init__(fn, ln)
-        self.fav_word = fav_word
+    def __init__(self, na, ml, hc):
+        super().__init__(na, ml)
+        self.hc = hc
