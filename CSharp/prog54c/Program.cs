0c0b90d61337cff3f21166a83ad838e9acb810ab
DbleTrouble <dbltble@gmail.com>
2025-04-11 14:49:02 -0500

    working

---
+++ CSharp/prog54c/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog54c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Radius: ");
+            int rad = int.Parse(Console.ReadLine());
+            double pie = 3.14159;
+            double area = pie * Math.Pow(rad,2);
+            double circum = 2 * pie * rad;
+            Console.WriteLine("Area: " + area);
+            Console.WriteLine("Circumference: " + circum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp/prog54d/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog54d
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Base: ");
+            double bas = int.Parse(Console.ReadLine());
+            Console.Write("Enter Height: ");
+            double hei = int.Parse(Console.ReadLine());
+            double area = (bas * hei) / 2;
+            double hypo = Math.Sqrt(Math.Pow(bas, 2) + Math.Pow(hei, 2));
+            double perim = bas + hei + hypo;
+            Console.WriteLine("Hypotenuse: " + Math.Round(hypo, 2));
+            Console.WriteLine("Area: " + Math.Round(area, 2));
+            Console.WriteLine("Perimeter: " + Math.Round(perim, 2));
+            Console.ReadLine();
+        }
+    }
+}
