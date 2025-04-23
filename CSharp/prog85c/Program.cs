4de974538eed26abf33f41bece3801b739293ebf
DbleTrouble <dbltble@gmail.com>
2025-04-23 15:27:54 -0500

    working

---
+++ CSharp/prog85c/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog85c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter birth month based on number 1 - 12: ");
+            int month = int.Parse(Console.ReadLine());
+            answer = month * 5) + 6) *4)
+        }
+    }
+}
