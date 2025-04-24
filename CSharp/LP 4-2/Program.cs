c9c6e7e667340f90e9e87c2cb0773a7a7f88664b
DbleTrouble <dbltble@gmail.com>
2025-04-24 14:44:28 -0500

    working

---
+++ CSharp/LP 4-2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LP_4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter package weight in kilograms: ");
+            int we = int.Parse(Console.ReadLine());
+            Console.Write("Enter package length in centimeters: ");
+            int l = int.Parse(Console.ReadLine());
+            Console.Write("Enter package width in centimeters: ");
+            int wi = int.Parse(Console.ReadLine());
+            Console.Write("Enter package height in centimeters: ");
+            int h = int.Parse(Console.ReadLine());
+
+            if ((we >= 27 && l >= 100000) || (we >= 27 && wi >= 100000) || (we >= 27 && h >= 100000))
+            {
+                Console.WriteLine("Too Heavy and Too Large");
+            }
+            else if (we >= 27)
+            {
+                Console.WriteLine("Too Heavy");
+            }
+            else if (l >= 100000)
+            {
+                Console.WriteLine("Too large");
+            }
+            else if (wi >= 100000)
+            {
+                Console.WriteLine("Too large");
+            }
+            else if (h >= 100000)
+            {
+                Console.WriteLine("Too large");
+            }
+            else
+            {
+                Console.WriteLine("The package is all good");
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- CSharp/prog85c/Program.cs
+++ CSharp/prog85c/Program.cs
@@ -12,7 +12,14 @@ namespace prog85c
         {
             Console.Write("Enter birth month based on number 1 - 12: ");
             int month = int.Parse(Console.ReadLine());
-            answer = month * 5) + 6) *4)
+            Console.Write("Enter birth day: ");
+            int day = int.Parse(Console.ReadLine());
+            double sa = ((((((month * 5) + 6) * 4) + 9) * 5) + day);
+            double a1 = Math.Floor((sa - 165) / 100);
+            double a2 = ((sa - 165) % 100);
+            Console.WriteLine("Your Number: " + sa);
+            Console.WriteLine("Brithday: " + a1 + "/" + a2);
+            Console.ReadLine();
         }
     }
 }
