9f492734f3d02aa0c2fab5cd51e93eaf1faaeca9
DbleTrouble <dbltble@gmail.com>
2025-05-16 15:22:03 -0500

    working

--- CSharp/StrInterview 18/Program.cs
+++ CSharp/StrInterview 18/Program.cs
@@ -4,28 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StrInterview_8
+namespace StrInterview_18
 {
     class Program
     {
-       
         static void Main(string[] args)
         {
             Console.Write("Enter word: ");
             string word = Console.ReadLine().ToLower();
-            Console.Write("Enter letter to find: ");
+            Console.Write("Enter letter to remove: ");
             char letter = Console.ReadLine().ToLower()[0];
-
-            int l = 0;
+            string word2 = "";
 
             for (int i = 0; i < word.Length; i++)
             {
                 if (word[i] == letter)
                 {
-                    l++;
+                    word2 = word.Remove(i,1);
                 }
             }
-            Console.WriteLine("The word is {0} the number of the letter '{1}' in the word is {2}", word, letter, l);
+            
+            Console.WriteLine("The origanl word is {0}, the letter removed is '{1}', the new word is {2}", word, letter, word2);
             Console.ReadLine();
         }
     }
